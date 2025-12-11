2c88a345e72b2387dc0e7fdf25648743b0bc8da3
milos.glogovac <milos.glogovac@gmail.com>
2025-12-11 17:10:21 +0100

    vps ip in swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -11,6 +11,7 @@ use OpenApi\Attributes as OA;
 #[
     OA\Info(version: "1.0.0", description: "Messenger backend", title: "Messenger backend api"),
     OA\Schema(format:"https"),
+    OA\Server(url: 'http://165.232.113.33/api', description: "Dev server"),
     OA\Server(url: 'http://nighthub.test/api', description: "Dev server"),
     OA\SecurityScheme( securityScheme: 'bearerAuth', type: "http", name: "Authorization", in: "header", scheme:"Bearer", description: "Authorize with bearer token"),
     OA\Contact(email: "tcom.developer@gmail.com")

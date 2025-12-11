fc3693661b683213fc45b5c552af104e75ed5554
milos.glogovac <milos.glogovac@gmail.com>
2025-12-11 16:54:36 +0100

    mail catcher test

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,22 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'pocetna';
+});
+
+Route::get('/test-mail', function () {
+    try {
+        \Mail::raw('Test email', function ($message) {
+            $message->to('test@example.com')
+                    ->subject('Test Email');
+        });   
+    } catch (\Exception $e) {
+        return response()->json([
+            'error' => $e->getMessage(),
+            'trace' => $e->getTraceAsString()
+        ]);
+    }
 });
